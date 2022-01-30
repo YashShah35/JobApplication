59af9724a42feb8f898f6aaa976e0612fd77d513
Yash.S <syash3599@gmail.com>
2022-01-30 10:07:43 +0530

    form updated

--- JobApplication/Program.cs
+++ JobApplication/Program.cs
@@ -4,6 +4,7 @@ using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -13,14 +14,14 @@ namespace JobApplication
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            new WebHostBuilder()
+        .UseKestrel()
+        .UseContentRoot(Directory.GetCurrentDirectory())
+        .UseStartup<Startup>()
+        .Build()
+        .Run();
         }
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
+       
     }
 }
--- JobApplication/Startup.cs
+++ JobApplication/Startup.cs
@@ -27,7 +27,7 @@ namespace JobApplication
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllers().AddSessionStateTempDataProvider();
+            services.AddControllersWithViews();
             services.AddDbContext<JobContext>(op => op.UseMySQL(Configuration.GetConnectionString("DbConnectionString")));
             services.AddDbContext<ApplicationContext>(options =>
                      options.UseMySQL(Configuration.GetConnectionString("DbConnectionString")));
